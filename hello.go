869d4c6537d3d458fee994cb964cbc92a915b506
yangliu618 <yangliu618@sina.com>
2017-02-13 07:48:33 +0800

    add http

--- hello.go
+++ hello.go
@@ -1,9 +1,16 @@
 package main
 
 import "fmt"
+import "net/http"
 
 func main() {
     fmt.Println("Hello, World!")
+    http.HandleFunc("/",hello)
+    http.ListenAndServe(":8080",nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("hello!"))
 }
 
 func init() {
