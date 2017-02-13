b41fb365b259168b5d9ae8823f6e68fc174bbbce
yangliu618 <yangliu618@sina.com>
2017-02-14 07:55:26 +0800

    add variable

--- hello.go
+++ hello.go
@@ -4,7 +4,23 @@ import "fmt"
 import "net/http"
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("Hello, World!" + " Jack")
+    fmt.Println("1+3=",1+3)
+    fmt.Println(true)
+
+    //字符串可以使用 + 来连接
+    fmt.Println("go" + "lang")
+
+    //整型和浮点数
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    //布尔类型，你可以使用布尔运算符
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+
+
     http.HandleFunc("/",hello)
     http.ListenAndServe(":8080",nil)
 }
